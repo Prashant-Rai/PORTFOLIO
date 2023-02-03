6766cd7d8b18684d43d47719a8ab81b7fef5fbcb
Prashant-Rai <prashantrai117@gmail.com>
2023-02-03 11:45:53 +0530

    fixed: css for education and experience section for mobile devices

--- src/components/ExpAndEdu.js
+++ src/components/ExpAndEdu.js
@@ -7,12 +7,17 @@ const ExperienceAndEducation = () => {
             <div className='container'>
                 <div className='row'>
                     <div className='col-6'>
-                    <h5 className='expHeader'>My Education</h5>
-                    <Experience />
+                        <div class='experienceContainer'>
+                            <h5 className='expHeader'>My Education</h5>
+                            <Experience />
+                            <div className='commonBorder isHidden'></div>
+                        </div>
                     </div>
                     <div className='col-6'>
+                    <div class='experienceContainer'>
                         <h5 className='expHeader'>My Education</h5>
                         <Experience />
+                    </div>
                     </div>
                 </div>
             </div>
